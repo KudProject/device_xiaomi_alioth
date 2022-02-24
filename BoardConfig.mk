f9fa507a927db16a179bb0d618908262ffa1995e
Albert I <kras@raphielgang.org>
2022-02-24 14:45:11 +0800

    alioth: Match target CPU variants with prebuilt vendor image
    Signed-off-by: Albert I <kras@raphielgang.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,17 +25,13 @@ include build/make/target/board/BoardConfigPixelCommon.mk
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a55
-TARGET_CPU_VARIANT_RUNTIME := cortex-a55
+TARGET_CPU_VARIANT := cortex-a76
 
-ifeq (,$(filter %_64,$(TARGET_PRODUCT)))
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
-endif
+TARGET_2ND_CPU_VARIANT := cortex-a76
 
 BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
